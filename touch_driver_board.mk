c965099484207024f41333c5f2d9c17fc82fed53
Zhenbin Tan <zhentan@qti.qualcomm.com>
2026-05-21 19:20:58 +0800

    touch: focaltech_ft3169: enable touch on pitti_wear
    Enable focaltech touch ft3169 on pitti_wear.
    
    Change-Id: I3c73cd09c0b049bf679a2fd6a0f00b00e465448a
    Signed-off-by: Zhenbin Tan <zhentan@qti.qualcomm.com>

--- touch_driver_board.mk
+++ touch_driver_board.mk
@@ -44,6 +44,7 @@ ifeq ($(TOUCH_DLKM_ENABLE),  true)
                                 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/synaptics_tcm_ts.ko
                         else ifeq ($(TARGET_BOARD_PLATFORM), pitti)
                                 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/focaltech_fts.ko \
+                                        $(KERNEL_MODULES_OUT)/focaltech_tp.ko \
                                         $(KERNEL_MODULES_OUT)/goodix_ts.ko \
                                         $(KERNEL_MODULES_OUT)/qts.ko
                         else ifeq ($(TARGET_BOARD_PLATFORM), volcano)
