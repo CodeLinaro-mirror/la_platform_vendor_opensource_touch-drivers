01c9463dc0d244404a9e737c0d1afce57791a78f
Shrikant Jagannath Patro <shripatr@qti.qualcomm.com>
2026-07-08 13:19:24 +0530

    touch-driver: Enable Atmel touchscreen on Lahaina cdp
    Enable Atmel MaxTouch touchscreen driver on Lahaina.
    
    Change-Id: I6740bc38549a5ed7db951b9c1f10415c70b646c3
    Signed-off-by: Shrikant Jagannath Patro <shripatr@qti.qualcomm.com>

--- Android.mk
+++ Android.mk
@@ -831,6 +831,17 @@ else ifeq ($(TARGET_BOARD_PLATFORM), lahaina)
        LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
        include $(DLKM_DIR)/Build_external_kernelmodule.mk
        ###########################################################
+
+	 ###########################################################
+       include $(CLEAR_VARS)
+       LOCAL_SRC_FILES   := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
+       LOCAL_MODULE              := atmel_mxt_ts.ko
+       LOCAL_MODULE_KBUILD_NAME  := atmel_mxt_ts.ko
+       LOCAL_MODULE_TAGS         := optional
+       #LOCAL_MODULE_DEBUG_ENABLE := true
+       LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+       include $(DLKM_DIR)/Build_external_kernelmodule.mk
+       ###########################################################
 else
        ###########################################################
        include $(CLEAR_VARS)
