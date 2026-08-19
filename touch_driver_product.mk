aa56af0561759f6dd7e7ce86c23cef7577af54a9
lliu6 <lliu6@qti.qualcomm.com>
2026-08-19 20:21:54 +0800

    touch-drivers: remove focaltech_fts.ko from pitti module lists
    Change-Id: I877fc6d80b740e14b9369eace545a4da676a8da9
    Signed-off-by: lliu6 <lliu6@qti.qualcomm.com>

--- touch_driver_product.mk
+++ touch_driver_product.mk
@@ -41,8 +41,7 @@ ifeq ($(TOUCH_DLKM_ENABLE),  true)
         else ifeq ($(TARGET_BOARD_PLATFORM), trinket)
                 PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/synaptics_tcm_ts.ko
         else ifeq ($(TARGET_BOARD_PLATFORM), pitti)
-                PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/focaltech_fts.ko \
-                        $(KERNEL_MODULES_OUT)/focaltech_tp.ko \
+                PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/focaltech_tp.ko \
                         $(KERNEL_MODULES_OUT)/goodix_ts.ko \
                         $(KERNEL_MODULES_OUT)/qts.ko
         else ifeq ($(TARGET_BOARD_PLATFORM), volcano)
